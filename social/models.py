24e2296b4b37c1b17422a944774056958dce35a4
maissaranejmi <123570071+maissaranejmi@users.noreply.github.com>
2023-02-01 15:08:04 +0100

    Add files via upload

---
+++ social/models.py
@@ -0,0 +1,20 @@
+
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    body = models.TextField()
+    image = models.ImageField(upload_to='uploads/post_photos', blank=True, null=True)
+    created_on = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    document = models.FileField(upload_to='documents/', blank=True, null=True)
+    
+
+
+class Comment(models.Model):
+    comment = models.TextField()
+    created_on = models.DateTimeField(default=timezone.now)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
